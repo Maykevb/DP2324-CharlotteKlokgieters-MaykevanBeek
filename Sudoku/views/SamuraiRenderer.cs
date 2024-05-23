67e18cf9c28d696f03748da427328fdfaae041c8
Mayke <mtmbeek@avans.nl>
2024-05-23 10:32:27 +0200

    code styling

--- Sudoku/views/SamuraiRenderer.cs
+++ Sudoku/views/SamuraiRenderer.cs
@@ -1,10 +1,5 @@
 ﻿using Sudoku.models.BoardComponent;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Timers;
 
 namespace Sudoku.renderers
 {
