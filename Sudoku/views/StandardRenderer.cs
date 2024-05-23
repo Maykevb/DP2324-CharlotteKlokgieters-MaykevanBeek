79ede72462197d9d2b7ae21137401cf7655259ec
Mayke <mtmbeek@avans.nl>
2024-05-23 09:59:19 +0200

    refactor standardrenderer

--- Sudoku/views/StandardRenderer.cs
+++ Sudoku/views/StandardRenderer.cs
@@ -9,7 +9,7 @@ namespace Sudoku.renderers
 {
     public class StandardRenderer : iBoardRenderer
     {
-		public object Clone()
+        public object Clone()
         {
             return new StandardRenderer();
         }
@@ -18,44 +18,48 @@ namespace Sudoku.renderers
 		{
 			double rowLength = Math.Sqrt(board.Cells.Count);
 
-			DrawSeparator(rowLength, squareLength);
+			DrawHorizontalSeparator(rowLength, squareLength);
 
 			for (int i = 0; i < board.Cells.Count; i++)
 			{
 				if (i % rowLength == 0)
 				{
-					Console.Write("|");
-				}
+					DrawVerticalSeperator();
+                }
 
 				DrawCell(board.Cells[i].Value);
 
 				if ((i + 1) % squareLength == 0 && !((i + 1) % rowLength == 0) || (i + 1) % (squareHeight * squareLength) == 0)
 				{
-					Console.Write('|');
+                    DrawVerticalSeperator();
+                }
+
+				if ((i + 1) % rowLength == 0 && (i + 1) % (squareHeight * rowLength) == 0)
+				{
+					DrawHorizontalSeparator(rowLength, squareLength);
+					continue;
 				}
 
-				if ((i + 1) % rowLength == 0 && (i + 1) % (squareHeight * rowLength) == 0) 
-				{
-					DrawSeparator(rowLength, squareLength);
-				}
-				else if ((i + 1) % rowLength == 0)
+				if ((i + 1) % rowLength == 0)
 				{
 					Console.WriteLine();
 				}
 			}
 		}
 
+		public void DrawVerticalSeperator()
+		{
+            Console.Write("|");
+        }
+
 		public void DrawCell(int value)
 		{
 			Console.Write(value == 0 ? " " : value.ToString());
 		}
 
-		public void DrawSeparator(double rowLength, int squareLength) 
+		public void DrawHorizontalSeparator(double rowLength, int squareLength) 
 		{
-            StringBuilder rowSeparator = new StringBuilder();
-			rowSeparator.AppendLine();
-            rowSeparator.Append(new string('-', (int)(rowLength + (rowLength / squareLength) + 1))); 
-            Console.WriteLine(rowSeparator.ToString());
-        }
+			Console.WriteLine("\n" + new string('-', (int)(rowLength + (rowLength / squareLength) + 1)));
+		}
 	}
 }
